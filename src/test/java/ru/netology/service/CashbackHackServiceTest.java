cb6862b2d9d994ffe274ffc09973e505f2b19cbc
AntonKopylov89 <ttxb@yandex.ru>
2022-07-18 22:32:26 +0400

    Add Junit4 testing framework

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+    public class CashbackHackServiceTest {
+
+        @Test
+        public void shouldRemain100() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 900;
+            int expected = 100;
+            int actual = service.remain(amount);
+
+            assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldRemain0() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1000;
+            int expected = 0;
+            int actual = service.remain(amount);
+
+            assertEquals(actual, expected);
+        }
+    }
