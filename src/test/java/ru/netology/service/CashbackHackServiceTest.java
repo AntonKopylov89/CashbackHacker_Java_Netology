e11394ec2bf7357aa3c130c65931cef14199d55d
AntonKopylov89 <ttxb@yandex.ru>
2022-07-19 20:14:13 +0400

    Add Junit Jupiter and Junit Vintage testing frameworks

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -25,4 +25,24 @@ import static org.junit.Assert.*;
 
             assertEquals(actual, expected);
         }
+
+        @org.junit.jupiter.api.Test
+        public void shouldRemain100API() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 900;
+            int expected = 100;
+            int actual = service.remain(amount);
+
+            assertEquals(actual, expected);
+        }
+
+        @org.junit.jupiter.api.Test
+        public void shouldRemain0API() {
+            CashbackHackService service = new CashbackHackService();
+            int amount = 1000;
+            int expected = 0;
+            int actual = service.remain(amount);
+
+            assertEquals(actual, expected);
+        }
     }
